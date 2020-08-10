4f465ddb8a8b804b46cc08d97b9d1d71f1774065
Matthew Zegar <mzegar@sfu.ca>
2020-08-09 18:47:59 -0700

    Updates

---
+++ precache-manifest.efbffb1672ab0cb4e7681e959bfb7dac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2beb8362410332117c7fba449301030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d5d5582f599723123a6",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "51d8d1e9ac588d17b8fe",
+    "url": "/static/css/main.5ac42ad1.chunk.css"
+  },
+  {
+    "revision": "1d5d5582f599723123a6",
+    "url": "/static/js/2.a1b71183.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a1b71183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d8d1e9ac588d17b8fe",
+    "url": "/static/js/main.a33d9ab3.chunk.js"
+  },
+  {
+    "revision": "5790c59e4cdfe64bd3e1",
+    "url": "/static/js/runtime-main.cb0fb42f.js"
+  },
+  {
+    "revision": "24bf6d256ff2a4e68eb3c783324b2e8a",
+    "url": "/static/media/matt.24bf6d25.png"
+  }
+]);
