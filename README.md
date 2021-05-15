4ef4f42079df541368fbc329e1da00ef159eaa5e
mzegar <matthewzegar@gmail.com>
2021-05-14 20:41:51 -0700

    Push old website

---
+++ README.md
@@ -0,0 +1,22 @@
+# mzegar.github.io
+
+Personal portfolio website created using React
+
+https://mzegar.github.io/
+
+## Project Structure
+
+- src/...
+  - main.js - Main webpage file
+  - src/components/...
+    - body.js - Body for the webpage
+    - header.js - Header for the webpage
+    - navbar.js - Navigation bar for the webpage
+    
+## Building
+
+`npm install`
+    
+## Deployment
+
+`yarn run deploy`
---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import {FaGithub, FaIdCard, FaLinkedinIn} from "react-icons/fa";
+import React from "react";
+
+export function NavBar() {
+    return (
+        <ul className="nav justify-content-center">
+            <NavBarItem link={'https://github.com/mzegar/resume/blob/master/Matthew%20Zegar%20-%20Resume%202020%201page.pdf'}>
+                <FaIdCard className={'navBarItemIcon'}/> Resume
+            </NavBarItem>
+
+            <NavBarItem link={'https://github.com/mzegar'}>
+                <FaGithub className={'navBarItemIcon'}/> GitHub
+            </NavBarItem>
+
+            <NavBarItem link={'https://www.linkedin.com/in/matthewzegar/'}>
+                <FaLinkedinIn className={'navBarItemIcon'}/> LinkedIn
+            </NavBarItem>
+        </ul>
+    );
+}
+
+function NavBarItem(props) {
+    return (
+        <div className={'navBarItemSpacing'}>
+            <a href={props.link}>
+                <button type="button" className="btn btn-outline-light">
+                    {props.children}
+                </button>
+            </a>
+        </div>
+    );
+}
